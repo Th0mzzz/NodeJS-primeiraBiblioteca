3cfea202d18aa1da55cd013768110935a1c3c8db
Th0mzzz <contatothomazvmendes@gmail.com>
2024-01-18 22:04:00 -0300

    att

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log("Olá Mundo")
-console.log(chalk.blue('este texto está azul'))
+
+
+function extractLinks(text) {
+    const regex = /https?:\/\/[\w]*.[^\s\)]*/gm;
+    const captures = [...text.matchAll(regex)]
+    const results = captures.map(capture => ({ [capture[1]]: capture[2] }))
+    return results
+}
+
+function solveError(error) {
+    console.log(error)
+    throw new Error(chalk.red(error.code, '###ERROR###'))
+}
+
+async function catchFile(path) {
+    try {
+        const encoding = 'utf-8'
+        const result = await fs.promises.readFile(path, encoding)
+        console.log(extractLinks(result))
+    } catch (error) {
+        solveError(error)
+    }
+}
+
+catchFile("./arquivos/texto.md");
+
+
